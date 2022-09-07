1fdfe3890b12c95794dfdfc646c6b6a0658bccd0
Daniel-Roa <lion35528@gmail.com>
2022-09-07 10:54:31 -0500

    add step enter credentials

--- src/test/java/com/co/qvision/stepsdefinitions/StepsDefinitionUserUpdate.java
+++ src/test/java/com/co/qvision/stepsdefinitions/StepsDefinitionUserUpdate.java
@@ -1,5 +1,8 @@
 package com.co.qvision.stepsdefinitions;
 
+import com.co.qvision.models.Credentials;
+import com.co.qvision.tasks.Home;
+import com.co.qvision.tasks.MyAccount;
 import cucumber.api.java.Before;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
@@ -10,6 +13,8 @@ import net.serenitybdd.screenplay.actors.OnStage;
 import net.serenitybdd.screenplay.actors.OnlineCast;
 import net.thucydides.core.annotations.Managed;
 import org.openqa.selenium.WebDriver;
+
+import java.util.List;
 
 public class StepsDefinitionUserUpdate {
     // Here instance the web driver that we use
@@ -28,8 +33,14 @@ public class StepsDefinitionUserUpdate {
     }
 
     @When("^enter my correct credentials$")
-    public void enterMyCorrectCredentials() {
-
+    public void enterMyCorrectCredentials(List<Credentials> credentialsList) {
+        // Call the method tha navigate to the module my account
+        OnStage.theActorInTheSpotlight().attemptsTo(Home.seeMyAccount());
+        // Save the credentials on the model credentials object
+        Credentials credentials;
+        credentials = credentialsList.get(0);
+        // Call the method to enter crendentials
+        OnStage.theActorInTheSpotlight().attemptsTo(MyAccount.enterCredentilas(credentials));
     }
 
     @When("^change my personal data$")
