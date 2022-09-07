23efdfea63ffd249a28162e6cb7411f1a80214b6
MelannyJutinico <melannyjutinico@gmail.com>
2022-09-07 14:56:24 -0500

    Shoes Page - mapping the selectors ramdom of products

--- src/main/java/com/co/qvision/tasks/Cart.java
+++ src/main/java/com/co/qvision/tasks/Cart.java
@@ -12,17 +12,16 @@ public class Cart implements Task {
     @Override
     public <T extends Actor> void performAs(T actor) {
         actor.attemptsTo(Click.on(IndexPage.BTN_SHOES));
-        try {
-            Thread.sleep(2000);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
         actor.attemptsTo(Click.on(ShoesPage.BTN_SIZE));
+        actor.attemptsTo(Click.on(ShoesPage.BTN_PURCHASE));
         try {
             Thread.sleep(2000);
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
+        actor.attemptsTo(Click.on(ShoesPage.BTN_CART));
+
+
     }
 
     public static Cart add(){
--- src/main/java/com/co/qvision/userinterfaces/ShoesPage.java
+++ src/main/java/com/co/qvision/userinterfaces/ShoesPage.java
@@ -1,15 +1,27 @@
 package com.co.qvision.userinterfaces;
 
 import net.serenitybdd.screenplay.targets.Target;
+import org.apache.poi.ss.formula.functions.T;
 
 public class ShoesPage {
 
-    static int aleatorySize = (int) Math.floor(Math.random()*(5-1+1)+1);
+    //SELECT A RANDOM PRODUCT FOR VALIDATE
 
-    public static final Target BTN_SIZE = Target.the("select random button of shoes")
-            .locatedBy("//ul[@class=\"products columns-3\"]/li[1]/div/div/div/div[3]/div[@class=\"size \"]["+aleatorySize+"]");
-    public static final Target BTN_SIZE1 = Target.the("Input for select size")
-            .locatedBy("");
-    public static final Target BTN_PURCHASE1 = Target.the("button to buy shoes ")
-            .locatedBy("//input[@id=\"username\"]");
+    //get a random integer number from 1 to 3 for shoe size
+    static int aleatorySize = (int) Math.floor(Math.random()*(3-1+1)+1);
+
+    //get a random integer from 1 to 10 to select the product
+    static int aleatoryProduct = (int) Math.floor(Math.random()*(10-1+1)+1);
+
+    //the button of a random size of a random product is selected and clicked
+    public static final Target BTN_SIZE = Target.the("random size button of shoes")
+            .locatedBy("//ul[@class=\"products columns-3\"]/li["+aleatoryProduct+"]/div/div/div/div[@class=\"sizes_options\"]/div[@class=\"size \"]["+aleatorySize+"]");
+    //the button purchase of a random product is selected and clicked
+    public static final Target BTN_PURCHASE = Target.the("random button buy of shoes ")
+            .locatedBy("//ul[@class=\"products columns-3\"]/li["+aleatoryProduct+"]/div/div/div[2]/a");
+
+    //the button open the shopping card is clicked
+    public static final Target BTN_CART = Target.the("button open shopping cart")
+            .locatedBy("//p/a[@href=\"https://www.bon-bonite.com/carrito/\"]");
+
 }
