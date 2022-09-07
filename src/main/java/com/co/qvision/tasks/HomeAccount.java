f76e89ed058c7f5fd2b4d8f7cb7bc632a6abc364
Daniel-Roa <lion35528@gmail.com>
2022-09-07 16:56:58 -0500

    update home account and user interface

---
+++ src/main/java/com/co/qvision/tasks/HomeAccount.java
@@ -0,0 +1,22 @@
+package com.co.qvision.tasks;
+
+import com.co.qvision.userinterfaces.HomePage;
+import net.serenitybdd.screenplay.Actor;
+import net.serenitybdd.screenplay.Task;
+import net.serenitybdd.screenplay.Tasks;
+import net.serenitybdd.screenplay.actions.Click;
+
+public class HomeAccount implements Task {
+    @Override
+    public <T extends Actor> void performAs(T actor) {
+        // Click on the button burger to open menu
+        actor.attemptsTo(Click.on(HomePage.BTN_OPEN_MENU));
+        // Click on the option my account
+        actor.attemptsTo(Click.on(HomePage.BTN_MI_ACCOUNT_MENU));
+
+    }
+    public static HomeAccount seeMyAccount(){
+        return Tasks.instrumented(HomeAccount.class);
+    }
+
+}
