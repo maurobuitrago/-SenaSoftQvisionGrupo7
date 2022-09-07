2db1be5806e2257eb04798a48dc3623fe0a22e3b
MelannyJutinico <melannyjutinico@gmail.com>
2022-09-07 11:50:50 -0500

    created cart runner and the basis of step defininitions

---
+++ src/test/java/com/co/qvision/stepsdefinitions/CartStepDefinitions.java
@@ -0,0 +1,23 @@
+package com.co.qvision.stepsdefinitions;
+
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class CartStepDefinitions {
+
+    @When("^i add a product to the cart$")
+    public void iAddAProductToTheCart() {
+
+    }
+
+    @When("^increased its units$")
+    public void increasedItsUnits() {
+
+    }
+
+    @Then("^should be able to see your final price$")
+    public void shouldBeAbleToSeeYourFinalPrice() {
+
+    }
+
+}
