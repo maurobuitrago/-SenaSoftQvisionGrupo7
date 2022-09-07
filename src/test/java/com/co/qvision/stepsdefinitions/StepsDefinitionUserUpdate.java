7c8c0e797ed05abcb7213a20b011eaf5ce4d4789
Daniel-Roa <lion35528@gmail.com>
2022-09-07 09:20:17 -0500

    Update StepsDefinitionUserUpdate.java check open the url bon bonite

--- src/test/java/com/co/qvision/stepsdefinitions/StepsDefinitionUserUpdate.java
+++ src/test/java/com/co/qvision/stepsdefinitions/StepsDefinitionUserUpdate.java
@@ -1,13 +1,30 @@
 package com.co.qvision.stepsdefinitions;
 
+import cucumber.api.java.Before;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+import net.serenitybdd.screenplay.abilities.BrowseTheWeb;
+import net.serenitybdd.screenplay.actions.Open;
+import net.serenitybdd.screenplay.actors.OnStage;
+import net.serenitybdd.screenplay.actors.OnlineCast;
+import net.thucydides.core.annotations.Managed;
+import org.openqa.selenium.WebDriver;
 
 public class StepsDefinitionUserUpdate {
+    // Here instance the web driver that we use
+    @Managed
+    WebDriver chrome;
+    @Before
+    public void setUp(){
+        OnStage.setTheStage(new OnlineCast());
+        OnStage.theActorCalled("User");
+        OnStage.theActorInTheSpotlight().can(BrowseTheWeb.with(chrome));
+    }
+
     @Given("^That i sign in my account on the option account detail$")
     public void thatISignInMyAccountOnTheOptionAccountDetail() {
-
+        OnStage.theActorInTheSpotlight().wasAbleTo(Open.url("https://www.bon-bonite.com/"));
     }
 
     @When("^enter my correct credentials$")
